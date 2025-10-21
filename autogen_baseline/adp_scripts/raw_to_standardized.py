2bd21f061e6ccb791c227e834b7646fe10c99129
zaidsheikh <sheikh.zaid@gmail.com>
2025-10-21 12:41:43 -0400

    add final response without thinking content

--- autogen_baseline/adp_scripts/raw_to_standardized.py
+++ autogen_baseline/adp_scripts/raw_to_standardized.py
@@ -26,12 +26,29 @@ def convert_step(step: dict[str, str]) -> list[Action | Observation]:
         ]
 
 
+def remove_thinking_content(text: str) -> str:
+    return text.split("</think>")[-1].strip()
+
+
 for line in sys.stdin:
     raw_data = json.loads(line)
 
     content = []
     for step in raw_data["messages"]:
         content.extend(convert_step(step))
+
+    final_message = raw_data["response"]["choices"][0]["message"]
+    assert final_message["role"] == "assistant"
+
+    final_response = remove_thinking_content(final_message["content"])
+
+    content += [
+        TextObservation(
+            content=final_response,
+            source="agent",
+            name=None,
+        )
+    ]
 
     standardized_data = Trajectory(id=raw_data["datum_id"], content=content)
     print(standardized_data.model_dump_json())
